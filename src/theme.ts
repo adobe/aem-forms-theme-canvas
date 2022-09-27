bfadc909dcb5fd824d3ce6537096ce050137977e
Amit Kumar <amitkumar@adobe.com>
2022-09-27 17:14:57 +0530

    implement canvas theme (#1)
    * implement canavs theme
    
    * remove questionmark css
    
    Co-authored-by: Amit Kumar <amitkumar@Amits-MacBook-Pro-8.local>

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,3 @@
 // Stylesheets
 import "./theme.scss";
 import "./resources/images/favicon.png"
-
-// Component Javascript
-import "./components/experiencefragment/change_flag.js";
-import "./components/form-options/form_dropdown.js";
-import "./components/progressbar/progressbar.js";
