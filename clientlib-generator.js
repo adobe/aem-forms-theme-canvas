c36d3d3453d5189244cd1d7c97bc2401f0985a5c
Suryansh Sharma <suryshar@adobe.com>
2023-05-08 12:22:29 +0530

    FORMS-7983 Add support to create clientlib in canvas theme and update it in aem 65

--- clientlib-generator.js
+++ clientlib-generator.js
@@ -3,7 +3,7 @@ var path = require("path");
 var process = require("process");
 
 
-const CLIENTLIB_DIR = process.env.npm_config_directory || "libs/fd/af/themes/corecomponents/canvas3-theme/clientlibs/"
+const CLIENTLIB_DIR = process.env.npm_config_directory || "theme-clientlibs"
 const CLIENTLIB_CATEGORY = process.env.npm_config_category || "adaptiveform.theme.canvas3"
 
 
@@ -11,7 +11,7 @@ clientlib(
   [
     {
       categories: [CLIENTLIB_CATEGORY],
-      name: "forms-clientlib-site",
+      name: "canvas3",
       cssProcessor: ["default:none", "min:none"],
       jsProcessor: ["default:none", "min:gcc;compilationLevel=whitespace"],
       allowProxy: true,
